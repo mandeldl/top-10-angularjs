713f00db83a855694e5b1b332161769728789174
David Mandel <MandelDL@gmail.com>
2015-03-06 16:38:39 -0500

    try copying and pasting from codecademy source

--- js/services/shows.js
+++ js/services/shows.js
@@ -1,10 +1,9 @@
 app.factory('shows', ['$http', function($http) {
-	return
-		$http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/shows-api/shows.json')
-		.success(function(data) {
-			return data;
-		})
-		.error(function(err) {
-			return err;
-		});
+  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/shows-api/shows.json') 
+            .success(function(data) { 
+              return data; 
+            }) 
+            .error(function(err) { 
+              return err; 
+            }); 
 }]);
